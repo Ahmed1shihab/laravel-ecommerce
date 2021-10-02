c449b6e9be86817494feae6609c68f2930323a58
Ahmed <ahmad12344321527@gmil.com>
2021-10-02 23:06:19 +0300

    edit `filesystems` config file

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('public'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
@@ -67,7 +67,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storage') => storage_path('public'),
     ],
 
 ];
