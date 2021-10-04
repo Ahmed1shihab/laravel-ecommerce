8a7f50e108f7ab416f313eafab6fe2f8443fe3a1
Ahmed <ahmad12344321527@gmil.com>
2021-10-04 15:39:19 +0300

    disable edit profile and remove `vue js`

--- resources/views/user_dashboard/dashboard.blade.php
+++ resources/views/user_dashboard/dashboard.blade.php
@@ -9,30 +9,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="{{ route('user.update') }}" method="post">
+                    <div class="mb-2 text-lg font-semibold flex justify-center">
+                        <p>You can't edit profile in demo version</p>
+                    </div>
+                    {{-- <form action="{{ route('user.update') }}" method="post">
                         {{ csrf_field() }}
-                        {{ method_field('PUT') }}
+                        {{ method_field('PUT') }} --}}
 
                         <div>
                             <p>Name</p>
-                            <input type="text" name="name" value="{{ auth()->user()->name }}" class="block w-72  shadow border-l-2 border-r-0 border-t-0 border-b-0 border-dblue focus:ring-0 focus:shadow-md ">
+                            <input readonly type="text" name="name" value="{{ auth()->user()->name }}" class="bg-gray-100 text-gray-600 block w-72  shadow border-l-2 border-r-0 border-t-0 border-b-0 border-dblue focus:ring-0 focus:shadow-md ">
                         </div>
 
                         <div class="mt-4">
                             <p>Email</p>
-                            <input type="email" name="email" value="{{ auth()->user()->email }}" class="block w-72  shadow border-l-2 border-r-0 border-t-0 border-b-0 border-dblue focus:ring-0 focus:shadow-md ">
+                            <input readonly type="email" name="email" value="{{ auth()->user()->email }}" class="bg-gray-100 text-gray-600 block w-72  shadow border-l-2 border-r-0 border-t-0 border-b-0 border-dblue focus:ring-0 focus:shadow-md ">
                         </div>
 
                         <div class="mt-4">
                             <p>Password</p>
-                            <input type="password" name="password" class="block w-72 shadow border-l-2 border-r-0 border-t-0 border-b-0 border-dblue focus:ring-0 focus:shadow-md ">
+                            <input readonly type="password" name="password" class="bg-gray-100 text-gray-600 block w-72 shadow border-l-2 border-r-0 border-t-0 border-b-0 border-dblue focus:ring-0 focus:shadow-md ">
                             <span class="text-xs">Keep it empty if you won't to change password</span>
                         </div>
 
                         <div class="mt-4">
-                            <button type="submit" class="bg-dblue text-white p-2 rounded transition-all duration-300 hover:bg-opacity-90">Update Profile 👽</button>
+                            <button type="submit" disabled class="bg-dblue text-white cursor-not-allowed p-2 rounded transition-all duration-300 hover:bg-opacity-90">Update Profile 👽</button>
                         </div>
-                    </form>
+                    {{-- </form> --}}
                 </div>
             </div>
         </div>
