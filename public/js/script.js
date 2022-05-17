c8a854b9b153fb393b60b0573563fc8905139f4c
Ahmed <ahmad12344321527@gmil.com>
2022-05-17 14:54:03 +0300

    remove unused script

--- public/js/script.js
+++ public/js/script.js
@@ -6,9 +6,3 @@ burger.addEventListener("click", () => {
     burger.classList.toggle("open");
     document.getElementById("mobile-stuff").classList.toggle("open");
 });
-
-// const addToCart = document.getElementById("add-to-cart");
-
-// addToCart.addEventListener("submit", function (e) {
-//     e.preventDefault;
-// });
